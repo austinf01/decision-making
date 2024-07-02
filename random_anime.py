eaf29463a45105b988e069ae678d4a5b17fc4912
Austin <austin.fong@sjsu.edu>
2024-07-02 07:21:32 -0700

    Added moods to help select what to watch with user input

--- random_anime.py
+++ random_anime.py
@@ -1,5 +1,19 @@
 from random import choice
 
-animes = ['naruto', 'haikyuu', 'mushishi', 'bleach']
+moods = ['sad', 'angry', 'fear', 'romance', 'calm', 'happy', 'anxious', 'cheerful', 'dissapointed', 'disgusted', 'energetic', 'bored']
 
-print(choice(animes))
+animes = [['Naruto', 'happy'],
+          ['Haikyuu', 'sad', ], 
+          ['Mushishi', 'energetic'],
+          ['Bleach', 'angry']]
+
+selection = []
+
+print('what mood are you in?')
+mood = input()
+
+for item in animes:
+  if item[1] == mood:
+    selection.append(item[0])
+
+print('\nYou should watch ' + choice(selection))
