6d4cfbb1c2cd2e871d432ad7bd22ae13f7b73dd5
Austin <austin.fong@sjsu.edu>
2024-07-02 22:15:45 -0700

    Added dataframe via pandas to find title, number of episodes, and the anime genres

--- random_anime.py
+++ random_anime.py
@@ -1,6 +1,8 @@
 from jikanpy import Jikan
 from random import choice
 
+import pandas as pd
+
 moods = ['sad', 'angry', 'fear', 'romance', 'calm', 'happy', 'anxious', 'cheerful', 'dissapointed', 'disgusted', 'energetic', 'bored']
 
 animes = [['Naruto', 'happy'],
@@ -11,13 +13,29 @@ animes = [['Naruto', 'happy'],
 selection = []
 
 jikan = Jikan()
-print(jikan.anime(457))
+df = pd.DataFrame(jikan.anime(457))
+print(df['data'][['title_japanese', 'title_english']])
+print(df['data']['episodes'])
 
-print('what mood are you in?')
-mood = input()
+#gets the number of minutes in the duration of the episode/movie 
+print([int(s) for s in df['data']['duration'].split() if s.isdigit()][0])
 
-for item in animes:
-  if item[1] == mood:
-    selection.append(item[0])
+#gets genre
+# print([genre['name'] for genres_list in df['data'].apply(lambda x: x['genres']) for genre in genres_list])
 
-print('\nYou should watch ' + choice(selection))
+# List comprehension to extract 'name' from 'genres'
+
+genres = df['data']['genres']
+
+size = len(genres)
+names = [genres[i]['name'] for i in range(size)]
+print(names)
+
+# print('what mood are you in?')
+# mood = input()
+
+# for item in animes:
+#   if item[1] == mood:
+#     selection.append(item[0])
+
+# print('\nYou should watch ' + choice(selection))
